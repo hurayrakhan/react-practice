cfc86a786073ae4355754d6df0c628e5accaef3a
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-03-24 14:49:53 +0600

    learn to add react component and add styles

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,21 @@ function App() {
   return (
     <>
       <h1>Hello React</h1>
+      <br />
+      <Developer name='Abu Hurayra Khan' passion='Student' level='Hons 3rd' goal='To be a Full-stack Developer'></Developer>
     </>
+  )
+}
+
+
+function Developer (props) {
+  return(
+    <div style={{borderRadius:'20px',border:'2px solid skyblue'}}>
+      <h3>Name: {props.name}</h3>
+      <p>Passion: {props.passion}</p>
+      <p>Education level: {props.level}</p>
+      <p>Goal: {props.goal}</p>
+    </div>
   )
 }
 
