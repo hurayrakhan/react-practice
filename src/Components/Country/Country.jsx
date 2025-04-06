2de5a6a0a4867d72786d52210a55a502c0a89475
Abu Hurayra Khan <imhurayrakhan@gmail.com>
2025-04-06 19:40:59 +0600

    set count of visited countries and a list

--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -3,18 +3,18 @@ import './country.css'
 
 
 
-const Country = ({country}) => {
+const Country = ({country, handleVisitedCountries}) => {
     const [visit, setVisit] = useState(false)
 
     const handleVisit = () => {
     setVisit(!visit);
+    handleVisitedCountries(country);
     }
 
-    console.log(country)
     return (
         <div className={`country ${visit && 'visited'}`}>
             <h3>Name: {country.name.common}</h3>
-            <img src={country.flags.png} alt="" />
+            <img style={{height:'200px', width:'100%'}} src={country.flags.png} alt="" />
             <p>Independent: {country.independent ? 'Free' : 'Not Free'}</p>
             <button onClick={handleVisit}>{visit ? 'Visited' : 'Visit'}</button>
         </div>
--- src/Components/countries.jsx
+++ src/Components/countries.jsx
@@ -1,17 +1,37 @@
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from './Country/Country';
 import './countries.css'
 
 
 const Countries = ({countriesPromise}) => {
+    const [visited, setVisited] = useState([]);
+
     const countries = use(countriesPromise);
+
+    const handleVisitedCountries = (country) => {
+        console.log('I have been visited')
+        const newVisitedCountries = [...visited,country];
+        setVisited(newVisitedCountries);
+    }
 
     return (
         <div>
-            <h3>Hello World</h3>
+            <h2>Hello World</h2>
+            <h3>Visited countries: {visited.length}</h3>            
+            <h4>List of Visited countries:</h4>
+            <ol>
+                {
+                visited.map(country => <li>{country.name.common}</li>)
+                }
+            </ol>    
+            
             <div className='countries'>
                 {
-                    countries.map(country => <Country key={country.flag} country={country}></Country>)
+                    countries.map(country => <Country 
+                        key={country.flag}
+                        country={country}
+                        handleVisitedCountries={handleVisitedCountries}>
+                        </Country>)
                 }
             </div>
         </div>
